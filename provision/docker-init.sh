2e3d43fe728fb37df65cd70380ea121a97a7e056
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2016-04-03 02:43:33 +0800

    Update docker-init.sh

--- provision/docker-init.sh
+++ provision/docker-init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export DOCKER_HOST=tcp://192.168.56.2:2375
-#export DOCKER_TLS_VERIFY=
+export DOCKER_HOST=tcp://192.168.56.2:2376
+export DOCKER_TLS_VERIFY=1
 
 echo "
 # Docker settings
-export DOCKER_HOST=tcp://192.168.56.2:2375
-#export DOCKER_TLS_VERIFY=
+export DOCKER_HOST=tcp://192.168.56.2:2376
+export DOCKER_TLS_VERIFY=1
 " >> ~/.bashrc >> ~/.zshrc
